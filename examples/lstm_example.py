be641b5e9771056c9926a45cca37a9024276a1f1
hkv <12495975+hkveeranki@users.noreply.github.com>
2024-06-26 23:52:17 +0100

    Updates after library changes

--- examples/lstm_example.py
+++ examples/lstm_example.py
@@ -3,7 +3,7 @@ This example demonstrates how to use `LSTM` model from
 `speechemotionrecognition` package
 """
 
-from keras.utils import np_utils
+from keras.src.utils import to_categorical
 
 from common import extract_data
 from speechemotionrecognition.dnn import LSTM
@@ -12,20 +12,20 @@ from speechemotionrecognition.utilities import get_feature_vector_from_mfcc
 
 def lstm_example():
     to_flatten = False
-    x_train, x_test, y_train, y_test, num_labels = extract_data(
-        flatten=to_flatten)
-    y_train = np_utils.to_categorical(y_train)
-    y_test_train = np_utils.to_categorical(y_test)
-    print('Starting LSTM')
-    model = LSTM(input_shape=x_train[0].shape,
-                 num_classes=num_labels)
+    x_train, x_test, y_train, y_test, num_labels = extract_data(flatten=to_flatten)
+    y_train = to_categorical(y_train)
+    y_test_train = to_categorical(y_test)
+    print("Starting LSTM")
+    model = LSTM(input_shape=x_train[0].shape, num_classes=num_labels)
     model.train(x_train, y_train, x_test, y_test_train, n_epochs=50)
     model.evaluate(x_test, y_test)
-    filename = '../dataset/Sad/09b03Ta.wav'
-    print('prediction', model.predict_one(
-        get_feature_vector_from_mfcc(filename, flatten=to_flatten)),
-          'Actual 3')
+    filename = "../dataset/Sad/09b03Ta.wav"
+    print(
+        "prediction",
+        model.predict_one(get_feature_vector_from_mfcc(filename, flatten=to_flatten)),
+        "Actual 3",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     lstm_example()
